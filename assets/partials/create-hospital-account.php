711d46ccd26e4fdf16ab09e7dbe364d5f526c1e1
Farhan <imranjamil482@gmail.com>
2024-06-11 13:47:52 +0500

    Undefined array key
    there is an error on line 33,34 and 35 called undefined array key name,email,password

--- assets/partials/create-hospital-account.php
+++ assets/partials/create-hospital-account.php
@@ -27,3 +27,39 @@
         <button type="submit" name="submit" class="btn btn-fill btn-primary">Create Account</button>
     </div>
 </form>
+
+<?php
+if($_SERVER['REQUEST_METHOD'] =='POST'){
+    $name = $_POST['name'];
+    $email = $_POST ['email'];
+    $pass = $_POST['password'];
+
+$servername = "localhost";
+$username = "root";
+$passowrd = ""; 
+$database = "safeguarddb";
+
+$conn = mysqli_connect($servername,$username,$passowrd,$database);
+
+if(!$conn){
+    die("Sorry we failed to connect".mysqli_connect_error());
+}
+
+
+
+$sql ="INSERT INTO `hospitals`(`hostpital_name`, `hospital_email`, `hospital_password`) VALUES ('$name','$email','$pass')";
+$result= mysqli_query($conn,$sql);
+
+
+if(!$result){
+    echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
+  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+  <div>
+     Error 
+  </div>
+</div>';
+}
+
+
+}
+?>
